0340ba01fff69ce777d1f921d078b6eeae8754d2
Peter Medbury <peter.medbury@dingogap.net.au>
2023-08-19 13:22:00 +1000

    updated category model

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { STRING } = require('sequelize');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define columns - id & category_name
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+      }
   },
   {
     sequelize,
