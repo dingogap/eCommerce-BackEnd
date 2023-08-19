e1811222ecbb12882cb7030d9405f7242b30b290
Peter Medbury <peter.medbury@dingogap.net.au>
2023-08-19 18:09:11 +1000

    added post, put & destroy routes for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,19 @@ const { Category, Product } = require("../../models");
 router.get("/", (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+      },
+    ],
+  }).then((categoryData) => {
+    if (!categoryData) {
+      res.status(404).send();
+      return;
+    }
+    res.status(200).json(categoryData);
+  });
 });
 
 router.get("/:id", (req, res) => {
@@ -18,11 +31,11 @@ router.get("/:id", (req, res) => {
       },
     ],
   }).then((categoryData) => {
-    if (categoryData) {
+    if (!categoryData) {
       res.status(404).send();
       return;
     }
-    res.json(categoryData);
+    res.status(200).json(categoryData);
   });
 });
 
@@ -32,19 +45,43 @@ router.post("/", (req, res) => {
     category_name: req.body.categoryName,
   })
     .then((newCategory) => {
-      res.json(newCategory);
+      res.status(200).json(newCategory);
     })
     .catch((err) => {
-      res.json(err);
+      res.status(500).json(err);
     });
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      name: req.body.name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedCategory) => {
+      // Sends the updated book as a json response
+      res.status(200).json(updatedCategory);
+    })
+    .catch((err) => res.status(404).json(err));
 });
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.body.id,
+    },
+  })
+    .then((deletedCategory) => {
+      res.status(200).json(deletedCategory);
+    })
+    .catch((err) => res.status(404).json(err));
 });
 
 module.exports = router;
